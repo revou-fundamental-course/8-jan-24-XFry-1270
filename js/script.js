1d3b72279ed2ac937c0ff6fdaf5f776596a457ac
XFry <feryralexandria@gmail.com>
2024-01-16 18:07:48 +0700

    commit push js css

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let keliling = document.querySelector(".inputklg");
+
+document.querySelector('.hk').addEventListener('click',function(){
+    var show = keliling.value * 4;
+    document.querySelector(".klg").innerHTML=show;
+})
+document.querySelector(".rk").addEventListener('click',function(){
+    document.querySelector(".klg").innerHTML='Hasil';
+    keliling.value="";
+})
+
+let luas = document.querySelector(".inputL");
+
+document.querySelector('.kl').addEventListener("click",function(){
+    var show = luas.value*luas.value;
+    document.querySelector(".ls").innerHTML=show;
+})
+
+document.querySelector(".rl").addEventListener('click',function(){
+    document.querySelector(".ls").innerHTML='Hasil';
+    luas.value="";
+})
