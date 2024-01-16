34c211bce2b379021d20c9d0fd7fba0230e3ccca
XFry <feryralexandria@gmail.com>
2024-01-16 21:34:11 +0700

    Menambahkan alert js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let keliling = document.querySelector(".inputklg");
 
 document.querySelector('.hk').addEventListener('click',function(){
+    if(keliling.value == ""){
+        alert("Harap isi Nilai sisi !")
+        document.querySelector(".klg").innerHTML='Hasil';
+    }else{
     var show = keliling.value * 4;
     document.querySelector(".klg").innerHTML=show;
+    }
 })
 document.querySelector(".rk").addEventListener('click',function(){
     document.querySelector(".klg").innerHTML='Hasil';
@@ -12,6 +17,10 @@ document.querySelector(".rk").addEventListener('click',function(){
 let luas = document.querySelector(".inputL");
 
 document.querySelector('.kl').addEventListener("click",function(){
+    if(luas.value == ""){
+        alert("Harap isi Nilai sisi !")
+        document.querySelector(".ls").innerHTML="Hasil";
+    }
     var show = luas.value*luas.value;
     document.querySelector(".ls").innerHTML=show;
 })
